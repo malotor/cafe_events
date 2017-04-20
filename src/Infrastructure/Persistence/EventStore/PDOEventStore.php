c119fdf735de9d0a455a64594b356125c6881732
Manel <malotor@gmail.com>
2017-04-20 23:07:48 +0200

    Temporal commit

--- src/Infrastructure/Persistence/EventStore/PDOEventStore.php
+++ src/Infrastructure/Persistence/EventStore/PDOEventStore.php
@@ -9,13 +9,66 @@ use Buttercup\Protects\IdentifiesAggregate;
 class PDOEventStore implements EventStore
 {
 
-    public function commit(DomainEvents $events)
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct($pdo, $serializer)
     {
-        // TODO: Implement commit() method.
+        $this->pdo = $pdo;
+        $this->serializer = $serializer;
     }
 
+    /**
+     * @param DomainEvents $events
+     * @return void
+     */
+    public function commit(DomainEvents $events)
+    {
+        $stmt = $this->pdo->prepare(
+            'INSERT INTO events (aggregate_id, `type`, created_at, `data`)
+             VALUES (:aggregate_id, :type, :created_at, :data)'
+        );
+
+        foreach ($events as $event) {
+            $stmt->execute([
+                ':aggregate_id' => (string) $event->getAggregateId(),
+                ':type'         => get_class($event),
+                ':created_at'   => (new DateTimeImmutable())->format('Y-m-d H:i:s'),
+                ':data'         => $this->serializer->serialize($event, 'json')
+            ]);
+        }
+    }
+
+    /**
+     * @param IdentifiesAggregate $id
+     * @return AggregateHistory
+     */
     public function getAggregateHistoryFor(IdentifiesAggregate $id)
     {
-        // TODO: Implement getAggregateHistoryFor() method.
+        $stmt = $this->pdo->query(
+            'SELECT * FROM events WHERE aggregate_id = :aggregate_id'
+        );
+        $stmt->execute([':aggregate_id' => (string) $id]);
+
+        $events = [];
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $events[] = $this->serializer->deserialize(
+                $row['data'],
+                $row['type'],
+                'json'
+            );
+        }
+
+        $stmt->closeCursor();
+
+        return new AggregateHistory($id, $events);
     }
 }
